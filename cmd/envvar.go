0e8f763adf06470c9ab2884a8bb0ba946e0f55a3
Kris Nóva <kris@nivenly.com>
2022-06-05 06:00:52 -0400

    Save! Automatic commit from github.com/kris-nova/bin/git-save
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- cmd/envvar.go
+++ cmd/envvar.go
@@ -15,3 +15,59 @@
 \*===========================================================================*/
 
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Auth struct {
+	hackmdToken string
+	hackmdID    string
+
+	//discordToken   string
+	//discordChannel string
+	//
+	//twitterApiKey            string
+	//twitterApiKeySecret      string
+	//twitterBearerToken       string
+	//twitterAccessToken       string
+	//twitterAccessTokenSecret string
+	//
+	//mastodonClientID     string
+	//mastodonClientSecret string
+	//mastodonAccessToken  string
+	//mastodonUsername     string
+	//mastodonPassword     string
+	//mastodonServer       string
+}
+
+var (
+	auth     = &Auth{}
+	registry = []*EnvVar{
+		{
+			Name:        "LIVE_HACKMD_TOKEN",
+			Value:       "",
+			Destination: &auth.hackmdToken,
+			Required:    false,
+		},
+	}
+)
+
+type EnvVar struct {
+	Name        string
+	Value       string
+	Destination *string
+	Required    bool
+}
+
+func LoadEnvironmentalVariables() error {
+	for _, v := range registry {
+		v.Value = os.Getenv(v.Name)
+		if v.Required && v.Value == "" {
+			return fmt.Errorf("empty env var [%s]", v.Name)
+		}
+		*v.Destination = v.Value
+	}
+	return nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -17,25 +17,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"hash/fnv"
 	"os"
 	"time"
 
 	"github.com/kris-nova/livemd"
-
-	"github.com/kris-nova/livemd/pkg/twitter"
-
-	"github.com/kris-nova/livemd/pkg/mastodon"
-
-	"github.com/kris-nova/livemd/pkg/notify"
-
-	"github.com/kris-nova/livemd/pkg/discord"
-
-	"github.com/kris-nova/livemd/pkg/embedmd"
-
-	"github.com/kris-nova/livemd/pkg/hackmd"
 
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
@@ -50,36 +36,7 @@ var cfg = &AppOptions{}
 type AppOptions struct {
 	verbose  bool
 	filename string
-
-	hackmdToken string
-	hackmdID    string
-
-	discordToken   string
-	discordChannel string
-
-	twitterApiKey            string
-	twitterApiKeySecret      string
-	twitterBearerToken       string
-	twitterAccessToken       string
-	twitterAccessTokenSecret string
-
-	mastodonClientID     string
-	mastodonClientSecret string
-	mastodonAccessToken  string
-	mastodonUsername     string
-	mastodonPassword     string
-	mastodonServer       string
 }
-
-// # Edit ./livemd.md
-// live stream <title>    # Create a new live stream (hackmd)
-// live stream push       # Sync local changes to hackmd
-// live stream pull       # Overwrite local changes to hackmd
-//
-// # Update firebot with new hackmd URL (TODO Automate)
-// live twitch push       # Sync local file to twitch API
-// live twitch pull       # Overwrite local changes from twitch API
-// live twitch export     # Export twitch episode to YouTube
 
 func main() {
 	/* Change version to -V */
@@ -110,41 +67,42 @@ Use this program to perform tasks with Twitch, Hackmd, and YouTube.`,
 				Usage:     "Send notifications to configured backends.",
 				UsageText: "live notify <message>",
 				Action: func(c *cli.Context) error {
-					message := c.Args().Get(0)
-					if message == "" {
-						fmt.Println(livemd.Banner())
-						cli.ShowSubcommandHelp(c)
-						return nil
-					}
-					notifier := notify.New(message)
-					logrus.Infof("=== Starting Notification Bus ===")
-					var err error
-					if cfg.discordToken != "" {
-						err = notifier.EnableDiscord(cfg.discordToken, cfg.discordChannel)
-						if err != nil {
-							return fmt.Errorf("failed enabling discord: %v", err)
-						}
-					}
-					if cfg.twitterApiKeySecret != "" {
-						err = notifier.EnableTwitter(cfg.twitterAccessToken, cfg.twitterAccessTokenSecret, cfg.twitterApiKey, cfg.twitterApiKeySecret)
-						if err != nil {
-							return fmt.Errorf("failed enabling twitter: %v", err)
-						}
-					}
+					//message := c.Args().Get(0)
+					//if message == "" {
+					//	fmt.Println(livemd.Banner())
+					//	cli.ShowSubcommandHelp(c)
+					//	return nil
+					//}
+					//notifier := notify.New(message)
+					//logrus.Infof("=== Starting Notification Bus ===")
+					//var err error
+					//if cfg.discordToken != "" {
+					//	err = notifier.EnableDiscord(cfg.discordToken, cfg.discordChannel)
+					//	if err != nil {
+					//		return fmt.Errorf("failed enabling discord: %v", err)
+					//	}
+					//}
+					//if cfg.twitterApiKeySecret != "" {
+					//	err = notifier.EnableTwitter(cfg.twitterAccessToken, cfg.twitterAccessTokenSecret, cfg.twitterApiKey, cfg.twitterApiKeySecret)
+					//	if err != nil {
+					//		return fmt.Errorf("failed enabling twitter: %v", err)
+					//	}
+					//}
+					//
+					//if cfg.mastodonUsername != "" {
+					//	err = notifier.EnableMastodon(cfg.mastodonServer, cfg.mastodonAccessToken, cfg.mastodonClientID, cfg.mastodonClientSecret, cfg.mastodonUsername, cfg.mastodonPassword)
+					//	if err != nil {
+					//		return fmt.Errorf("failed enabling mastodon: %v", err)
+					//	}
+					//}
+					//
+					//// Run the notifications system
+					//err = notifier.Notify()
+					//logrus.Infof("=== Stopping Notification Bus ===")
+					//if err != nil {
+					//	return err
+					//}
 
-					if cfg.mastodonUsername != "" {
-						err = notifier.EnableMastodon(cfg.mastodonServer, cfg.mastodonAccessToken, cfg.mastodonClientID, cfg.mastodonClientSecret, cfg.mastodonUsername, cfg.mastodonPassword)
-						if err != nil {
-							return fmt.Errorf("failed enabling mastodon: %v", err)
-						}
-					}
-
-					// Run the notifications system
-					err = notifier.Notify()
-					logrus.Infof("=== Stopping Notification Bus ===")
-					if err != nil {
-						return err
-					}
 					return nil
 				},
 			},
@@ -161,30 +119,7 @@ Use this program to perform tasks with Twitch, Hackmd, and YouTube.`,
 						UsageText:   "live stream <title>",
 						Description: "Use this sync from remote.",
 						Flags:       GlobalFlags([]cli.Flag{}),
-						Action: func(c *cli.Context) error {
-							x, err := livemd.FromLocal(cfg.filename)
-							if err != nil {
-								return fmt.Errorf("unable to find local: %s: %v", cfg.filename, err)
-							}
-							client := hackmd.New(cfg.hackmdToken)
-							if cfg.hackmdID == "" {
-								return fmt.Errorf("empty hackmd id")
-							}
-							err = x.Sync(cfg.filename)
-							if err != nil {
-								return fmt.Errorf("sync: %v", err)
-							}
-							_, err = client.GetNote(cfg.hackmdID)
-							if err != nil {
-								// Does not exist
-								return fmt.Errorf("unable to find note: %s: %v", cfg.hackmdID, err)
-							}
-							_, err = client.UpdateNote(x.ToHackMD(cfg.hackmdID))
-							if err != nil {
-								return fmt.Errorf("unable to push: %v", err)
-							}
-							logrus.Infof("Saved hackmd   : %s", cfg.hackmdID)
-							logrus.Infof("Saved filename : %s", cfg.filename)
+						Action: func(c *cli.Context) error { //
 							return nil
 						},
 					},
@@ -195,44 +130,6 @@ Use this program to perform tasks with Twitch, Hackmd, and YouTube.`,
 						Description: "Use this command to overwrite remote.",
 						Flags:       GlobalFlags([]cli.Flag{}),
 						Action: func(c *cli.Context) error {
-							client := hackmd.New(cfg.hackmdToken)
-							if cfg.hackmdID == "" {
-								return fmt.Errorf("empty HACKMD_ID")
-							}
-							y, err := client.GetNote(cfg.hackmdID)
-							if err != nil {
-								return fmt.Errorf("unable to find local: %s: %v", cfg.filename, err)
-							}
-							x := &livemd.LiveMD{}
-							err = embedmd.Unmarshal([]byte(y.Content), x)
-							if err != nil {
-								return fmt.Errorf("invalid remote, unable to Unmarshal: %v", err)
-							}
-							x.SyncRaw([]byte(y.Content))
-							err = x.Write(cfg.filename)
-							if err != nil {
-								return fmt.Errorf("unable to write: %v", err)
-							}
-							logrus.Infof("Saved hackmd   : %s", cfg.hackmdID)
-							logrus.Infof("Saved filename : %s", cfg.filename)
-							return nil
-						},
-					},
-					{
-						Name:        "status",
-						Usage:       "Show status of local file.",
-						UsageText:   "live stream status",
-						Description: "Use this command to overwrite remote.",
-						Flags:       GlobalFlags([]cli.Flag{}),
-						Action: func(c *cli.Context) error {
-
-							// Status always comes from local state
-							x, err := livemd.FromLocal(DefaultFile)
-							if err != nil {
-								return fmt.Errorf("unable to open %s: %v", DefaultFile, err)
-							}
-							status := x.Status()
-							fmt.Print(status)
 							return nil
 						},
 					},
@@ -246,29 +143,6 @@ Use this program to perform tasks with Twitch, Hackmd, and YouTube.`,
 						return nil
 					}
 
-					// New with name
-					logrus.Infof("Creating New Stream \"%s\"", title)
-					x := livemd.New(title)
-					x.HackMDID = cfg.hackmdID
-					err := x.Write(cfg.filename)
-					if err != nil {
-						return fmt.Errorf("unable to write local: %v", err)
-					}
-					client := hackmd.New(cfg.hackmdToken)
-					note := x.ToHackMD(cfg.hackmdID)
-					if note.ID == "" {
-						logrus.Infof("Creating new hackMD note: %s", note.Title)
-						note, err = client.CreateNote(note)
-						x.HackMDID = note.ID
-						defer x.Write(cfg.filename)
-					} else {
-						logrus.Infof("Updating hackMD note: %s", note.Title)
-						note, err = client.UpdateNote(note)
-					}
-					if err != nil {
-						return fmt.Errorf("unable to save to hackmd: %v", err)
-					}
-					logrus.Infof("Successful save: %s", note.ID)
 					return nil
 				},
 			},
@@ -280,11 +154,16 @@ Use this program to perform tasks with Twitch, Hackmd, and YouTube.`,
 			return nil
 		},
 	}
-	Preloader()
-	err := Validation()
+	var err error
+	err = LoadEnvironmentalVariables()
 	if err != nil {
-		logrus.Errorf("Failed Validation: %v", err)
-		os.Exit(99)
+		logrus.Errorf("Failed Loading Environment: %v", err)
+		os.Exit(80)
+	}
+	if cfg.verbose {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.InfoLevel)
 	}
 	err = app.Run(os.Args)
 	if err != nil {
@@ -292,96 +171,6 @@ Use this program to perform tasks with Twitch, Hackmd, and YouTube.`,
 		os.Exit(1)
 	}
 	os.Exit(0)
-}
-
-func Validation() error {
-	cfg.hackmdToken = os.Getenv(hackmd.EnvironmentalVariableHackMDToken)
-	if cfg.hackmdToken == "" {
-		return fmt.Errorf("empty environmental variable [%s]", hackmd.EnvironmentalVariableHackMDToken)
-	}
-	return nil
-}
-
-// Preloader will run for ALL commands, and is used
-// to initalize the runtime environments of the program.
-func Preloader() {
-	/* Flag parsing */
-	if cfg.verbose {
-		logrus.SetLevel(logrus.DebugLevel)
-	} else {
-		logrus.SetLevel(logrus.InfoLevel)
-	}
-
-	// ---
-	// Environmental Variable Parsing
-	// ---
-
-	cfg.hackmdID = os.Getenv(hackmd.EnvironmentalVariableHackMDID)
-	if cfg.hackmdID != "" {
-		logrus.Infof(" --> Loading HackMD ID: %s", cfg.hackmdID)
-	}
-
-	cfg.discordToken = os.Getenv(discord.EnvironmentalVariableDiscordToken)
-	if cfg.discordToken != "" {
-		logrus.Infof(" --> Loading Discord Token: **********")
-	}
-
-	cfg.discordChannel = os.Getenv(discord.EnvironmentalVariableDiscordChannel)
-	if cfg.discordToken != "" {
-		logrus.Infof(" --> Loading Discord Channel: %s", cfg.discordChannel)
-	}
-
-	cfg.twitterApiKey = os.Getenv(twitter.EnvironmentalVariableTwitterAPIKey)
-	if cfg.twitterApiKey != "" {
-		os.Setenv("GOTWI_API_KEY", cfg.twitterApiKey)
-		logrus.Infof(" --> Loading Twitter API Key: %s", cfg.twitterApiKey)
-	}
-
-	cfg.twitterApiKeySecret = os.Getenv(twitter.EnvironmentalVariableTwitterAPIKeySecret)
-	if cfg.twitterApiKey != "" {
-		os.Setenv("GOTWI_API_KEY_SECRET", cfg.twitterApiKeySecret)
-		logrus.Infof(" --> Loading Twitter API Key Secret: **********")
-	}
-
-	cfg.twitterAccessToken = os.Getenv(twitter.EnvironmentalVariableTwitterAccessToken)
-	if cfg.twitterAccessToken != "" {
-		logrus.Infof(" --> Loading Twiter Access Token: **********")
-	}
-
-	cfg.twitterAccessTokenSecret = os.Getenv(twitter.EnvironmentalVariableTwitterAccessTokenSecret)
-	if cfg.twitterApiKeySecret != "" {
-		logrus.Infof(" --> Loading Twiter Access Token Secret: **********")
-	}
-
-	cfg.mastodonServer = os.Getenv(mastodon.EnvironmentalVariableMastodonServer)
-	if cfg.mastodonServer != "" {
-		logrus.Infof(" --> Loading Mastodon Server: %s", cfg.mastodonServer)
-	}
-
-	cfg.mastodonClientID = os.Getenv(mastodon.EnvironmentalVariableMastodonClientID)
-	if cfg.mastodonClientID != "" {
-		logrus.Infof(" --> Loading Mastodon Client ID: %s", cfg.mastodonClientID)
-	}
-
-	cfg.mastodonClientSecret = os.Getenv(mastodon.EnvironmentalVariableMastodonClientSecret)
-	if cfg.mastodonClientSecret != "" {
-		logrus.Infof(" --> Loading Mastodon Client Secret: **********")
-	}
-
-	cfg.mastodonAccessToken = os.Getenv(mastodon.EnvironmentalVariableMastodonAccessToken)
-	if cfg.mastodonAccessToken != "" {
-		logrus.Infof(" --> Loading Mastodon Access Token: **********")
-	}
-
-	cfg.mastodonUsername = os.Getenv(mastodon.EnvironmentalVariableMastodonUsername)
-	if cfg.mastodonUsername != "" {
-		logrus.Infof(" --> Loading Mastodon Username: %s", cfg.mastodonUsername)
-	}
-
-	cfg.mastodonPassword = os.Getenv(mastodon.EnvironmentalVariableMastodonPassword)
-	if cfg.mastodonPassword != "" {
-		logrus.Infof(" --> Loading Mastodon Password: **********")
-	}
 }
 
 func GlobalFlags(c []cli.Flag) []cli.Flag {
@@ -402,23 +191,4 @@ func GlobalFlags(c []cli.Flag) []cli.Flag {
 		c = append(c, gf)
 	}
 	return c
-}
-
-func Compare(a, b *hackmd.Note) bool {
-
-	h1 := hash(string(a.Content))
-	h2 := hash(string(b.Content))
-	logrus.Infof("Compare %d:%d", h1, h2)
-	return h1 == h2
-}
-
-func hash(s string) uint32 {
-	h := fnv.New32a()
-	h.Write([]byte(s))
-	return h.Sum32()
-}
-
-func Pause() {
-	fmt.Print("Press any key to continue...")
-	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
